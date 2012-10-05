01b19b7b5fc0d68377cc4c6b9259c1a7f7a08306
CivicResound <joshua.micah.towns@gmail.com>
2012-10-05 11:09:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7907870dcf653db39dab16a8db02640dea865c2c8758002d9d7f4f558a55682908132f368d1ab17288c97352634e85150d4eaaf8b0fd9a40e7f926162dfe9983'
