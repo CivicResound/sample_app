97a5c855678177c773ef7623ba8fba17fca4570d
CivicResound <joshua.micah.towns@gmail.com>
2012-11-02 22:15:42 -0400

    Delete unwanted migration

--- db/migrate/20121028171019_add_resume_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddResumeToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :resume_file_name, :string
-  	add_column :users, :resume_content_type, :string
-  	add_column :users, :resume_file_size, :integer
-  	add_column :users, :attachment_updated_at, :datetime
-  end
-end
