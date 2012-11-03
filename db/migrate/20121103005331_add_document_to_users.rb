9805bc80abcb06f63f37ea1017e000e9d7545e45
CivicResound <joshua.micah.towns@gmail.com>
2012-11-03 12:43:55 -0400

    Oh God Why

--- db/migrate/20121103005331_add_document_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddDocumentToUsers < ActiveRecord::Migration
-  def self.up
-    add_attachment :users, :document
-  end
-
-  def self.down
-    remove_attachment :users, :document
-  end
-end
